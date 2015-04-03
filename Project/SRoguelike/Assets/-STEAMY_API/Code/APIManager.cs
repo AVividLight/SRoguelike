dc641c637abbee5b8a48f82b815db56bc1799e15
Mr. Michael Gibson Bethke <gibsonbethke@gmail.com>
2015-04-02 23:12:27 -0500

    APIManager more extensible and clean

--- Project/SRoguelike/Assets/-STEAMY_API/Code/APIManager.cs
+++ Project/SRoguelike/Assets/-STEAMY_API/Code/APIManager.cs
@@ -28,11 +28,7 @@ public class Me
 
 
 public class APIManager : MonoBehaviour
-{
-	
-	internal APIKey apiKey;
-	internal Me me;
-	
+{	
 	
 	public User UserLogin ( string username, string password )
 	{
@@ -40,40 +36,58 @@ public class APIManager : MonoBehaviour
 		User user = new User ();
 		
 		RestClient client = new RestClient ( "https://steamy.io:443" );
+		
+		user.apikey = GetKey ( client, username, password );
+		user.me = GetMe ( client, user );
+		
+		return user;
+	}
+	
+	
+	private APIKey GetKey ( RestClient client, string username, string password )
+	{
+		
+		APIKey newKey = null;
+		
 		client.Authenticator = new HttpBasicAuthenticator ( username, password );
 
 		RestRequest apikeyRequest = new RestRequest ( "api/ApiKey", Method.POST );
 		apikeyRequest.RequestFormat = DataFormat.Json;
 		IRestResponse apikeyResponse = client.Execute ( apikeyRequest );
 		
-		apiKey = JsonConvert.DeserializeObject<APIKey> ( apikeyResponse.Content );
+		newKey = JsonConvert.DeserializeObject<APIKey> ( apikeyResponse.Content );
 		
-		if ( String.IsNullOrEmpty ( apiKey.id ) == true || String.IsNullOrEmpty ( apiKey.secret ) == true )
+		if ( String.IsNullOrEmpty ( newKey.id ) == true || String.IsNullOrEmpty ( newKey.secret ) == true )
 		{
 			
 			UnityEngine.Debug.Log ( "Unable to POST for APIKey, check Username and Password" );
 			return null;
 		}
 		
-		user.apikey = apiKey;
+		return newKey;
+	}
+	
+	
+	private Me GetMe ( RestClient client, User user )
+	{
 		
-		client.Authenticator = new HttpBasicAuthenticator ( apiKey.id, apiKey.secret );
+		Me newMe = null;
+		
+		client.Authenticator = new HttpBasicAuthenticator ( user.apikey.id, user.apikey.secret );
 		
 		RestRequest meRequest = new RestRequest ( "api/Me", Method.GET );
 		meRequest.RequestFormat = DataFormat.Json;
 		IRestResponse meResponse = client.Execute ( meRequest );
 		
-		me = JsonConvert.DeserializeObject<Me> ( meResponse.Content );
+		newMe = JsonConvert.DeserializeObject<Me> ( meResponse.Content );
 		
-		if ( String.IsNullOrEmpty ( me.userName ) == true )
+		if ( String.IsNullOrEmpty ( newMe.userName ) == true )
 		{
 			
 			UnityEngine.Debug.Log ( "Unable to GET for Me, this account could be corrupted!" );
 			return null;
 		}
 		
-		user.me = me;
-		
-		return user;
+		return newMe;
 	}
 }
